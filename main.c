b2507693e770268631f235902f8ccc44e627d443
Denis <ddd@ddd.com>
2023-04-05 11:39:58 +0300

    continue preproc

--- main.c
+++ main.c
@@ -28,11 +28,16 @@ void foo(int n, const std::string& nameN) {
 	std::cout << n << " " << nameN << std::endl;
 }
 
+#define B_test	A(100)
 int main(int argc, char** argv) {
 #ifdef RL6_21_2023
 	std::cout << "------------------\n";
 	std::cout << "start project" << std::endl;
 	std::cout << "------------------\n";
+#elif RL6_29_2023
+	std::cout << "defined RL6_29_2023" << std::endl;
+#else 
+	std::cout << "not defined RL6_21_2023" << std::endl;
 #endif
 
 
@@ -40,6 +45,7 @@ int main(int argc, char** argv) {
 	A1(test);
 	int temp1jlkldjflj = 543;
 	A(temp1jlkldjflj);
+	B_test;
 
 	return 0;
 }
